f8636a8d4a70256a3c18e0201ff6d712b08b5b90
Saul46 <saulov4664@gmail.com>
2026-03-31 11:42:22 -0600

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/test/java/com/ubam/isc/taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubam.isc.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
